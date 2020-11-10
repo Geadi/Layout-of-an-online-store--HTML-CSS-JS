8571a143620b774f32e517f22b1859b4be72062a
Geadi <66213778+Geadi@users.noreply.github.com>
2020-11-10 11:34:01 +0200

    add

--- start-n/js/main.js
+++ start-n/js/main.js
@@ -60,4 +60,8 @@ $(function(){
             normalFill: "#C4C4C4"
       });
 
+      $('.menu__btn').on('click', function(){
+        $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
+      });
+
 });
